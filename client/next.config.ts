4b0403a7e029800fc344960970f8d8fc09e7eea7
Shigosag (Segun Arulogun Gabriel) <88502810+Shigosag@users.noreply.github.com>
2026-09-26 17:10:07 +0000

    Update next.config.ts

--- client/next.config.ts
+++ client/next.config.ts
@@ -29,7 +29,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: process.env.SERVER_PROXY_URL || 'http://localhost:5000/api/:path*',
+        destination: process.env.SERVER_PROXY_URL || 'http://127.0.0.1:5000/api/:path*',
       },
     ];
   },
