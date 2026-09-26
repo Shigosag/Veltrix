8cbe7e9b9648f693bbbe0f2daca7c3dd2a883249
Shigosag (Segun Arulogun Gabriel) <88502810+Shigosag@users.noreply.github.com>
2026-09-26 17:05:43 +0000

    Update constants.ts

--- server/src/lib/constants.ts
+++ server/src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const AUTH_COOKIE = 'veltrix_session_token';
-export const PORT = Number(process.env.PORT || process.env.SERVER_PORT) || 5000;
+export const PORT = Number(process.env.SERVER_PORT) || 5000;
 export const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:3000';
 export const JWT_SECRET_STRING = process.env.JWT_SECRET || 'veltrix_production_secret_key_minimum_32_characters_long_sha256';
 
